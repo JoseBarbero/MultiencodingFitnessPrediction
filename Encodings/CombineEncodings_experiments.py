ca3ed7739e64ef2f0db52b48421e3c820bbb0672
josebarbero <jabarbero@ubu.es>
2023-03-15 14:15:40 +0100

    New arg parser

--- Encodings/CombineEncodings_experiments.py
+++ Encodings/CombineEncodings_experiments.py
@@ -52,10 +52,12 @@ def main(enc1, enc2, enc1_X, enc2_X, y, labeled_percentage, model, results_folde
     print("THREAD STARTED WITH ID: ", os.getpid())
     # Change regression labels to binary labels above first quartile and below
     original_y = y.copy()
+    
+    # TODO If I include this if, threads stop parallelizing
     if is_classifier(model):
         y = np.where(y >= np.percentile(y, 75), 1, 0).ravel()
     else:
-        y = original_y
+        y = y.copy().ravel()
 
     pred_dict_ct = dict()
     pred_dict_enc1 = dict()
