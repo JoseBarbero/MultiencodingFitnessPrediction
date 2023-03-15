8eb241db27ebbfbb8a2ea022d5e6feddb67448c0
josebarbero <jabarbero@ubu.es>
2023-03-15 17:47:32 +0100

    Extrapolation experiments

--- Encodings/CombineEncodings_extrapolation_experiments.py
+++ Encodings/CombineEncodings_extrapolation_experiments.py
@@ -362,7 +362,7 @@ if __name__ == "__main__":
     for variant in test_variants:
         test_indexes.extend(variants_dict[variant])
 
-    results_folder = results_folder.replace("experiments_", f"experiments_trainedwith{train_variants}_testedwith{test_variants}_")
+    results_folder = results_folder.replace("experiments_", f"experiments_trainedwith{'_'.join(train_variants)}_testedwith{'_'.join(test_variants)}_")
     
     print(f"* Total dict size: {round(sum([enc_X.nbytes for enc_X in encodings_dict.values()])/(1024*1024), 2)} MB | {round(sum([enc_X.nbytes for enc_X in encodings_dict.values()])/(1024*1024*1024), 2)} GB", flush=True)
     arguments = []
