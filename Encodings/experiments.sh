0617618494294965c3da6d58bde29c94db3a1e0e
josebarbero <jabarbero@ubu.es>
2023-03-29 12:39:57 +0200

    Individual masking gaussian filter

--- Encodings/experiments.sh
+++ Encodings/experiments.sh
@@ -4,6 +4,6 @@
 for dataset in ../data/*/
 do
     echo "Running Masking experiments for $dataset"
-    python Masking_experiments.py --data $dataset --cpus 32
+    python Masking_experiments.py --data $dataset --cpus 50
     #python Masking_extrapolation_experiments.py --data $dataset --cpus 32 --trainvariants 1 --testvariants 2
 done
